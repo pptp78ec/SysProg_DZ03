4df9ff6e9dfc12b44446811569997ac36442fb16
pptp78ec <pptp78ec@gmail.com>
2022-11-13 16:26:54 +0200

    first commit

---
+++ SysProg_DZ03/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SysProg_DZ03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    struct HorseResult
+    {
+        public string Horsename { set; get; }
+        public TimeSpan Time { set; get; }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public static object locker = new();
+        static Random random = new Random(DateTime.Now.Millisecond);
+        public MainWindow()
+        {
+            InitializeComponent();
+            horseResults = new();
+            LV_Results.ItemsSource = horseResults;
+        }
+
+        ObservableCollection<HorseResult> horseResults;
+        void RunHorsy(ProgressBar pb, string horsename)
+        {
+            double minvalue = 0, maxvalue = 0;
+            Dispatcher.InvokeAsync(delegate () { pb.Value = 0; minvalue = pb.Minimum; maxvalue = pb.Maximum; });
+            DateTime startTime = DateTime.Now;
+            int increment = random.Next(1, 10);
+            for (var i = minvalue; i <= maxvalue; i += increment)
+            {
+                Dispatcher.InvokeAsync(delegate () { pb.Value += i; });
+                Thread.Sleep(300);
+            }
+            DateTime endTime = DateTime.Now;
+            Dispatcher.InvokeAsync(delegate () { horseResults.Add(new HorseResult { Horsename = horsename, Time = endTime.Subtract(startTime) }); });
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Thread thread1 = new(() => RunHorsy(PB_Horsy_1, "Horse A"));
+            thread1.IsBackground = true;
+            Thread thread2 = new(() => RunHorsy(PB_Horsy_2, "Horse B"));
+            thread2.IsBackground = true;
+            Thread thread3 = new(() => RunHorsy(PB_Horsy_3, "Horse C"));
+            thread3.IsBackground = true;
+            Thread thread4 = new(() => RunHorsy(PB_Horsy_4, "Horse D"));
+            thread4.IsBackground = true;
+            Thread thread5 = new(() => RunHorsy(PB_Horsy_5, "Horse E"));
+            thread5.IsBackground = true;
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread4.Start();
+            thread5.Start();
+
+
+        }
+
+    }
+}
---
+++ SysProg_DZ03_2/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace SysProgDZ03_2 
+{
+    public  struct ThePair
+    {
+        public int first { get; set; }
+        public int second { get; set; }
+    }
+
+    public class Func
+    {
+        private static object locker = new();
+
+        private List<ThePair> pairs = new();
+        private static Random random = new(DateTime.Now.Millisecond);
+        private FileInfo fileInfo = new FileInfo("Output.txt");
+        private FileStream fs;
+        public void Generate()
+        {
+            lock (locker)
+            {
+                for (int i = 0; i < random.Next(5, 15); i++)
+                {
+                    pairs.Add(new ThePair() { first = random.Next(1, 10000), second = random.Next(1, 10000) });
+                }
+                try
+                {
+                    using (fs = new(fileInfo.FullName, FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        StreamWriter sw = new(fs) ;
+                        foreach (ThePair pair in pairs)
+                        {
+                            sw.WriteLine(pair.first.ToString() + " " + pair.second.ToString());
+                            Console.WriteLine("Generating: " + pair.first + " " + pair.second);
+                        }
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            }
+        }
+        public void Summ()
+        {
+            lock (locker)
+            {
+                List<int> intlist = new();
+                foreach (ThePair pair in pairs)
+                {
+                    intlist.Add(pair.first + pair.second);
+                }
+                try
+                {
+                    using (fs = new(fileInfo.FullName, FileMode.Append, FileAccess.Write))
+                    {
+                        StreamWriter sw = new(fs);
+                        sw.WriteLine();
+                        foreach (int i in intlist)
+                        {
+                            sw.WriteLine(i.ToString());
+                            Console.WriteLine("Generating sum result: " + i);
+                        }
+
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+            }
+        }
+        public void Mult()
+        {
+            lock (locker)
+            {
+                List<Int64> intlist = new();
+                foreach (ThePair pair in pairs)
+                {
+                    intlist.Add((Int64)(pair.first * pair.second));
+                }
+                try
+                {
+                    using (fs = new(fileInfo.FullName, FileMode.Append, FileAccess.Write))
+                    {
+                        StreamWriter sw = new(fs);
+                        sw.WriteLine();
+                        foreach (int i in intlist)
+                        {
+                            sw.WriteLine(i.ToString());
+                            Console.WriteLine("Generating mult result: " + i);
+                        }
+
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+            }
+        }
+    }
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Func func = new();
+            Thread thread1 = new(() => func.Generate());
+            Thread thread2 = new(() => func.Summ());
+            Thread thread3 = new(() => func.Mult());
+            thread1.Start();
+            thread1.Join();
+            thread2.Start();
+            thread2.Join();
+            thread3.Start();
+            thread3.Join();
+        }
+
+        
+
+    }
+}
